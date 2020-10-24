e5db94b4e17193d3325e3f0580970ec18d3b4fef
ElishevaMedioni <medioni@g.jct.ac.il>
2020-10-24 21:35:25 +0300

    Initial Code + Welcome 8390

---
+++ dotNet5781_00_8390_1366/Program8390.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_00_8390_1366
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome8390();
+            Welcome1366();
+            Console.ReadKey();
+        }
+        static partial void Welcome1366();
+        private static void Welcome8390()
+        {
+            Console.WriteLine("Enter your name: ");
+            string myStr = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", myStr);
+        }
+    }
+}
