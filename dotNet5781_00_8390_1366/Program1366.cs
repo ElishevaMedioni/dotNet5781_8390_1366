e51a870277502ceec049d8ef0e1084ca29cc59b8
KerenSamama <ksamama@g.jct.ac.il>
2020-10-24 22:35:48 +0300

    inial code+Welcome1366

---
+++ dotNet5781_00_8390_1366/Program1366.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_00_8390_1366
+{
+   partial class Program
+    {
+        static partial void Welcome1366()
+        {
+            Console.WriteLine("I am also here");
+        }
+    }
+}
