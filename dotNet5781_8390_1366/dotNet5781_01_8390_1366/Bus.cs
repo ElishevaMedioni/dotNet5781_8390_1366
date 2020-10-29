89098e6604ff4afcbd8a8270c4ec6f4e790aaf5f
ElishevaMedioni <medioni@g.jct.ac.il>
2020-10-29 15:56:54 +0200

    nouveau code

---
+++ dotNet5781_8390_1366/dotNet5781_01_8390_1366/Bus.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_01_8390_1366
+{
+    class Bus
+    {
+        private int licenseNum;
+        private static int kmNumGas = 0;
+        private static int kmNumTechnicalControl = 0;
+        DateTime dateOfActivity;
+
+        public Bus()
+        { // default constructor, initialyse
+            licenseNum = 0;
+            dateOfActivity = new DateTime(0, 0, 0);
+        }
+
+        public Bus(int myLicenseNum, DateTime mydateOfActivity)
+        {
+            licenseNum = myLicenseNum;
+            dateOfActivity = mydateOfActivity;
+        }
+
+        public int getLicenseNum()
+        {
+            return licenseNum;
+        }
+
+        public void printLicenseNum()
+        {
+            int numDigit = licenseNum.ToString().Length;
+
+            if (numDigit == 7) //if the beginning of the activity is before 2018 then the licenseNum is 7 digits
+                Console.WriteLine(licenseNum / 100000 + "-" + (licenseNum % 100000) / 100 + "-" + licenseNum % 100);
+
+            else //else the licenseNum is 8 digits
+                Console.WriteLine(licenseNum / 100000 + "-" + (licenseNum % 100000) / 1000 + "-" + licenseNum % 1000);
+        }
+    }
+}
--- dotNet5781_8390_1366/dotNet5781_01_8390_1366/Program.cs
+++ dotNet5781_8390_1366/dotNet5781_01_8390_1366/Program.cs
@@ -11,10 +11,6 @@ namespace dotNet5781_01_8390_1366
 {
     class Program
     {
-        //essaie1
-        //essaie2
-        //essaie3
-        //essaie4
         public enum MyEnum { addBus, programTravel, busSetting, display, exit };
 
         /// <summary>
@@ -29,7 +25,7 @@ namespace dotNet5781_01_8390_1366
             Console.WriteLine("Enter 3 to  display the km traveld/n");
             Console.WriteLine("Enter 4 to exit, bye bye/n");
         }
-        
+
         /// <summary>
         /// fonction to check if the license number that the user enter isn't already use to an other bus in  the system
         /// </summary>
@@ -38,7 +34,7 @@ namespace dotNet5781_01_8390_1366
         /// <returns>bool</returns>
         static public bool ExistBus(List<Bus> buses, int myLicenseNum)
         {
-           return buses.Exists(x => x.getLicenseNum() == myLicenseNum);
+            return buses.Exists(x => x.getLicenseNum() == myLicenseNum);
         }
 
 
@@ -47,7 +43,7 @@ namespace dotNet5781_01_8390_1366
         /// it receives the date and the license number
         /// </summary>
         /// <returns>Bus</returns>
-        static public Bus funcAddBus() 
+        static public Bus funcAddBus()
         {
             Console.WriteLine("Enter date of the beginning of your activity: /n " +
                 "Enter the day ");
@@ -67,7 +63,7 @@ namespace dotNet5781_01_8390_1366
 
             DateTime date1 = new DateTime(yearInt, monthInt, dayInt);
 
-            
+
             Console.WriteLine("Enter your license number: ");
             string licenseNum = Console.ReadLine();
             int licenseNumInt;
@@ -85,45 +81,47 @@ namespace dotNet5781_01_8390_1366
                 Bus b1 = new Bus(licenseNumInt, date1);
                 return b1;
             }
-            else { //if the license number format is wrong
-                
+            else
+            { //if the license number format is wrong
+
                 return null;
             }
         }
-            static void Main(string[] args)
-            {
-                List<Bus> buses = new List<Bus>();
+        static void Main(string[] args)
+        {
+            List<Bus> buses = new List<Bus>();
             int choice;
             printMenuOption();
 
-                while (int.TryParse(Console.ReadLine(), out choice))
-                    Console.Write("ERROR, entre un autre");
-                while (choice != 4)
+            while (int.TryParse(Console.ReadLine(), out choice))
+                Console.Write("ERROR, entre un autre");
+            while (choice != 4)
+            {
+                switch ((MyEnum)choice)
                 {
-                    switch ((MyEnum)choice)
-                    {
-                        case MyEnum.addBus: //add bus to the system
-                        if (funcAddBus()==null)
+                    case MyEnum.addBus: //add bus to the system
+                        if (funcAddBus() == null)
                         {
                             Console.WriteLine("ERROR the license number you entered is wrong/n");
                         }
-                        else {
+                        else
+                        {
                             Bus b1 = funcAddBus();
                             buses.Add(b1);
                         }
-                            break;
-                        case MyEnum.programTravel:
-                            break;
-                        case MyEnum.busSetting:
-                            break;
-                        case MyEnum.display:
-                            break;
+                        break;
+                    case MyEnum.programTravel:
+                        break;
+                    case MyEnum.busSetting:
+                        break;
+                    case MyEnum.display:
+                        break;
                     default:
                         Console.WriteLine("no such option");
-                            break;
-                    }
-                printMenuOption();
+                        break;
                 }
+                printMenuOption();
             }
         }
     }
+}
