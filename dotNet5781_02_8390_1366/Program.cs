ae921fa6d7797ad456540e03cc4b699176361a56
ElishevaMedioni <medioni@g.jct.ac.il>
2020-11-03 16:22:39 +0200

    Debut ex02

---
+++ dotNet5781_02_8390_1366/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_02_8390_1366
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
