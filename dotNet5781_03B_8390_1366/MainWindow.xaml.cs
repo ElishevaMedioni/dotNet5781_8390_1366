3023b203089a9e52f1464284aa1bb3827e434ba4
ElishevaMedioni <medioni@g.jct.ac.il>
2020-12-10 14:14:17 +0200

    syncro

--- dotNet5781_03B_8390_1366/MainWindow.xaml.cs
+++ dotNet5781_03B_8390_1366/MainWindow.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Collections.ObjectModel;
+using System.Windows.Threading;
+using System.Threading;
 
 
 namespace dotNet5781_03B_8390_1366
@@ -24,10 +26,12 @@ namespace dotNet5781_03B_8390_1366
     public partial class MainWindow : Window
          
     {
+        Bus current;
+        //public Bus SelectedItem { get; set; }
         public static List<Bus> buses = new List<Bus>();
         //static ListBuses buses = new ListBuses();
         public ObservableCollection<Bus> myCollection = new ObservableCollection<Bus>(buses);
-
+        private DispatcherTimer timer = new DispatcherTimer();
         //
         public MainWindow()
         {
@@ -37,7 +41,20 @@ namespace dotNet5781_03B_8390_1366
             //cbBusLines.DisplayMemberPath = " GetBusLineNum ";
             //cbBusLines.SelectedIndex = 0;
             
-            myListBox.ItemsSource = buses;
+            myListView.ItemsSource = buses;
+
+
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += timer_Tick;
+            timer.Start();
+        }
+
+
+        void timer_Tick(object sender, EventArgs e)
+        {
+            Thread.Sleep(1);
+            lblTime.Content = DateTime.Now.ToLongTimeString();
+
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -45,6 +62,42 @@ namespace dotNet5781_03B_8390_1366
             WindowToAddANewBus secondWindow = new WindowToAddANewBus(); 
             secondWindow.Show();
 
+        }
+
+       
+
+        private void myListView_MouseDoubleClick(object sender, MouseButtonEventArgs e) //doubleclick sur chaque bus, affiche fenetre ac pratim du bus
+        {
+            //if (myListView.SelectedItems.Count > 0)
+            //    MessageBox.Show(myListView.SelectedItems[0].ToString());
+            current = (Bus)myListView.SelectedItem;
+
+            ViewItem secondWindow = new ViewItem(current);
+            secondWindow.Show();
+            
+            myListView.Items.Refresh();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e) //boutton nouveau trajet a cote de chaque bus
+        {
+            if (sender != null && sender is Button btn)
+                current = (Bus)btn.DataContext;
+            //current = (Bus)myListView.SelectedItem;
+            NewTripWindow secondWindow = new NewTripWindow(current);
+            secondWindow.Show();
+            myListView.Items.Refresh();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e) //bouton refuel a cote de chaque bus
+        {
+            if (sender != null && sender is Button btn)
+                current = (Bus)btn.DataContext;
+           // current = (Bus)myListView.SelectedItem;
+            current.GetKmNumGas = 0;
+
+            MessageBox.Show("The Refueling Was Successfully Completed", "Important Message");
+            myListView.Items.Refresh();
+            
         }
     }
 }
---
+++ dotNet5781_03B_8390_1366/NewTripWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace dotNet5781_03B_8390_1366
+{
+    /// <summary>
+    /// Interaction logic for NewTripWindow.xaml
+    /// </summary>
+    public partial class NewTripWindow : Window
+    {
+        Bus newTripForThisBus;
+        public NewTripWindow(Bus myBus)
+        {
+            InitializeComponent();
+            newTripForThisBus = myBus;
+           // kmForTheTrip.KeyDown += new KeyEventHandler(kmForTheTrip_KeyDown);
+        }
+
+        private void kmForTheTrip_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+
+            
+            string item = this.kmForTheTrip.Text.ToString();
+
+            DateTime date1 = DateTime.Now;
+            DateTime date2 = newTripForThisBus.DateOfTheLastTechnicalControl;
+            TimeSpan t = date1 - date2;
+
+            bool flag = int.TryParse(item, out int kmFTT);
+
+            if (flag)
+            {
+
+                if (newTripForThisBus.GetKmNumGas + kmFTT > 1200)
+                {
+                    MessageBox.Show("ERROR YOU NEED TO FILL OIL", "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
+                    newTripForThisBus.Status = "On refueling";
+
+                }
+                else if ((newTripForThisBus.GetNumTechnicalControl + kmFTT > 20000) || Math.Round(t.TotalDays) > 375)
+                {
+                    MessageBox.Show("YOU NEED TO DO TECHNICAL VERIFICATION", "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
+
+                    newTripForThisBus.Status = "in traitement";
+
+                }
+
+                else if (newTripForThisBus.GetKmNumGas + kmFTT > 1200 && newTripForThisBus.GetNumTechnicalControl + kmFTT > 20000)
+                    newTripForThisBus.Status = "in traitement";
+
+                else
+                {
+                    newTripForThisBus.Status = "On the road again";
+                    newTripForThisBus.GetNumTechnicalControl += kmFTT;
+                    newTripForThisBus.GetKmNumGas += kmFTT;
+                    MessageBox.Show("THE NEW ITINERARY OF THE BUS HAS BEEN UPDATED SUCCESSFULLY FOR " + kmFTT + " kms","Important Message");
+                        kmForTheTrip.Clear();
+
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("Wrong format for the number of km", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+                this.Close();
+            }
+            
+        }
+    }
+}
